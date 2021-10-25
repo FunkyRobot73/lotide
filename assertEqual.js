55a53edbeaede9b3f0670820ccd5228305ecda70
vagrant <arlo@gmail.com>
2021-10-25 19:48:17 +0000

    Comparing using assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  //console.log(`Does ${actual} = ${expected} ??`)
+  //console.log((console.assert(actual === expected)));
+  if (actual === expected ? console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`) : console.log(`😈😈😈 Assertion Failed: [${actual}] !== [${expected}]`));
+  
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("jimmy", "jimmy");
+assertEqual("77", "77");
